40c2be755e76db29378680fe1ff461fb7d189c73
dianadiaz02 <80539135+dianadiaz02@users.noreply.github.com>
2022-03-11 18:33:17 -0600

    pequeños cambios

--- HashMap.java
+++ HashMap.java
@@ -1,2 +1,8 @@
+import java.util.HashMap;
+
 public class HashMap<K,V> implements Map<K,V> {
+    @Override
+    public void put() {
+
+    }
 }
--- LinkedHashMap.java
+++ LinkedHashMap.java
@@ -1,2 +1,8 @@
+import java.util.LinkedHashMap;
+
 public class LinkedHashMap<K,V> implements Map<K,V>{
+    @Override
+    public void put() {
+
+    }
 }
