87d01ff2b3e258230f4426a4bb31480d42ed9192
dianadiaz02 <80539135+dianadiaz02@users.noreply.github.com>
2022-03-11 18:54:28 -0600

    Update File.java

--- File.java
+++ File.java
@@ -1,2 +1,26 @@
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class File {
+
+    public File(String s) {
+
+    }
+    public class ReadFile {
+        public static void main(String[] args) {
+            try {
+                File archivo = new File("cards_desc.txt");
+                Scanner myReader = new Scanner(archivo);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
 }
