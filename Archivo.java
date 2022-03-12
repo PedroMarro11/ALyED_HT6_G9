284137fbeb481da92bc14179296620100639d66e
dianadiaz02 <80539135+dianadiaz02@users.noreply.github.com>
2022-03-12 12:20:49 -0600

    Update Archivo.java

--- Archivo.java
+++ Archivo.java
@@ -1,27 +1,21 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.CharBuffer;
-import java.util.Scanner;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
-public class File implements Readable {
-
-    public File() {
-
+public class Archivo {
+  
+  public void leerArchivo() {
+    try {
+      File archivo = new File("cards_desc.txt");
+      Scanner myReader = new Scanner(archivo);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
-    public class ReadFile {
-        public static void main(String[] args) throws FileNotFoundException {
-            File archivo = new File("cards_desc.txt");
-            Scanner myReader = new Scanner(archivo);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        }
-    }
-    @Override
-    public int read(CharBuffer cb) throws IOException {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+  }
 }
