ebba39dc08be1ee200385f23dc8997d554c63dc5
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-03-17 23:50:28 -0600

    encabezados

--- Controlador.java
+++ Controlador.java
@@ -1,3 +1,11 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos
+ * Hoja de Trabajo 6
+ * @author Diana Díaz 21066
+ * @author Pedro Marroquín 21801
+ * @version 1.0 17/03/2022
+ */
 public class Controlador {
     /**
      * Método para verificar que la entrada del usuario sea
