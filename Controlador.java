345b97809c3844fafeb4272ced606891a06e7cfc
Pedro Javier Marroquín Abrego <mar21801@uvg.edu.gt>
2022-03-17 20:51:48 -0600

    Update Controlador.java
    creación de método isInt para evaluar entradas de usuario

--- Controlador.java
+++ Controlador.java
@@ -1,2 +1,18 @@
 public class Controlador {
+    /**
+     * Método para verificar que la entrada del usuario sea
+     * un int
+     * @param entrada
+     * @return true si es un entero
+     */
+    public static boolean isInt(String entrada)
+    {
+       try {
+           Integer.parseInt(entrada);
+           return true;
+       }catch(Exception e)
+       {
+           return false;
+       }
+    }
 }
